792dde3fc242ba515b2039dec3f822b6e6a46952
Omar sabry <omarsabry@gmail.com>
2025-09-17 21:26:44 +0300

    connet to db

---
+++ src/common/database/database.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [
+        
+        ],
+        logging: ['error', 'warn', 'query'], // Logs queries in development
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
