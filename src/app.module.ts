49d7c5aaa16b0a86807bddf4d2b3e64dbe67d32f
Omar sabry <omarsabry@gmail.com>
2025-09-18 11:40:39 +0300

    Add Translation Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { DatabaseModule } from './common/database/database';
 import { GraphqlModule } from './common/graphql/graphql.module';
 import { ConfigModule } from './common/config/config.module';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TranslationModule } from './common/translation/translation.module';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule, ConfigModule, ThrottlerModule],
+  imports: [
+    ConfigModule,
+    GraphqlModule,
+    DatabaseModule,
+    ThrottlerModule,
+    TranslationModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
