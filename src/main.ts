d36300b373273d5f70382bddfb4c7df71fc85082
Omar sabry <omarsabry@gmail.com>
2025-09-18 11:25:24 +0300

    Add SqlInjectionInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,53 @@
-import { NestFactory } from '@nestjs/core';
+// import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
+// import * as bodyParser from 'body-parser';
+import { json } from 'express';
+import { DataSource } from 'typeorm';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { I18nValidationException } from 'nestjs-i18n';
 import { AppModule } from './app.module';
+import { GeneralResponseInterceptor } from './common/interceptors/generalResponse.interceptor';
+import { SqlInjectionInterceptor } from './common/interceptors/sqlInjection.interceptor';
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
+    // app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 5 }));
+    app.useGlobalInterceptors(
+      new ClassSerializerInterceptor(app.get(Reflector)),
+      new SqlInjectionInterceptor(),
+      new GeneralResponseInterceptor(),
+      new ClassSerializerInterceptor(app.get(Reflector)),
+    );
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        stopAtFirstError: true,
+        exceptionFactory: (errors) => new I18nValidationException(errors),
+      }),
+    );
+
+    // app.use('/google/callback', bodyParser.raw({ type: 'application/json' }));
+    app.use(json());
+
+    const dataSource = app.get(DataSource);
+    if (!dataSource.isInitialized) {
+      await dataSource.initialize();
+    }
+
+    await app.listen(process.env.PORT || 3000);
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestException(error);
+  }
 }
+
 bootstrap();
