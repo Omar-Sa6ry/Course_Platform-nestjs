74a98b8e9d10335a21b4654489ba9ab903d18b01
Omar sabry <omarsabry@gmail.com>
2025-09-18 11:32:35 +0300

    Add ThrottlerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database';
 import { GraphqlModule } from './common/graphql/graphql.module';
 import { ConfigModule } from './common/config/config.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule, ConfigModule],
+  imports: [DatabaseModule, GraphqlModule, ConfigModule, ThrottlerModule],
   providers: [AppService],
 })
 export class AppModule {}
