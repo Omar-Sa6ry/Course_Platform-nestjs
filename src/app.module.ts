04fad56a56c9d17f8e4789e41f1a3244234503e9
Omar sabry <omarsabry@gmail.com>
2025-09-18 11:28:49 +0300

    Add Config Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database';
 import { GraphqlModule } from './common/graphql/graphql.module';
+import { ConfigModule } from './common/config/config.module';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule],
+  imports: [DatabaseModule, GraphqlModule, ConfigModule],
   providers: [AppService],
 })
 export class AppModule {}
