61fd3d265f6adfebea153877de48d4a3bcf34b32
Omar sabry <omarsabry@gmail.com>
2025-09-18 11:38:19 +0300

    Add General Constant

---
+++ src/common/constant/enum.constant.ts
@@ -0,0 +1,39 @@
+import { registerEnumType } from '@nestjs/graphql';
+
+export enum Role {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+export const AllRoles: Role[] = Object.values(Role);
+
+export enum Permission {
+  UPDATE_USER = 'update_user',
+  DELETE_USER = 'delete_user',
+  EDIT_USER_ROLE = 'edit_user_role',
+
+  RESET_PASSWORD = 'RESET_PASSWORD',
+  CHANGE_PASSWORD = 'CHANGE_PASSWORD',
+  FORGOT_PASSWORD = 'FORGOT_PASSWORD',
+  LOGOUT = 'LOGOUT',
+
+  // Category
+  CREATE_CATEGORY = 'create_category',
+  UPDATE_CATEGORY = 'update_category',
+  DELETE_CATEGORY = 'delete_category',
+
+  // Cart
+  CREATE_CART = 'create_cart',
+  UPDATE_CART = 'update_cart',
+  DELETE_CART = 'delete_cart',
+  VIEW_CART = 'view_cart',
+}
+
+registerEnumType(Permission, {
+  name: 'Permission',
+  description: 'Detailed permissions in the system',
+});
+
+registerEnumType(Role, {
+  name: 'Role',
+  description: 'User roles in the system',
+});
---
+++ src/common/constant/rolePermissionsMap.constant.ts
@@ -0,0 +1,40 @@
+import { Permission, Role } from './enum.constant';
+
+export const rolePermissionsMap: Record<Role, Permission[]> = {
+  [Role.ADMIN]: [
+    // User
+    Permission.UPDATE_USER,
+    Permission.DELETE_USER,
+    Permission.EDIT_USER_ROLE,
+    Permission.RESET_PASSWORD,
+    Permission.CHANGE_PASSWORD,
+    Permission.FORGOT_PASSWORD,
+    Permission.LOGOUT,
+
+    // Category
+    Permission.CREATE_CATEGORY,
+    Permission.UPDATE_CATEGORY,
+    Permission.DELETE_CATEGORY,
+
+    // Cart
+    Permission.CREATE_CART,
+    Permission.UPDATE_CART,
+    Permission.DELETE_CART,
+    Permission.VIEW_CART,
+  ],
+
+  [Role.USER]: [
+    // User
+    Permission.UPDATE_USER,
+    Permission.RESET_PASSWORD,
+    Permission.CHANGE_PASSWORD,
+    Permission.FORGOT_PASSWORD,
+    Permission.LOGOUT,
+
+    // Cart
+    Permission.CREATE_CART,
+    Permission.UPDATE_CART,
+    Permission.DELETE_CART,
+    Permission.VIEW_CART,
+  ],
+};
